09000cc002bdb581737103c32eee6701636237ef
jghibiki <jghibiki.games@gmail.com>
2015-09-29 16:21:24 -0500

    add links to tweet text

--- app/static/js/view-models/side-pane-view-model.js
+++ app/static/js/view-models/side-pane-view-model.js
@@ -24,6 +24,40 @@ define(["ko", "jquery", "chain", "ol", "pipelineManager"], function(ko, jquery, 
         }
 
         self.selectedTweet = ko.observable()
+	self.computedText = ko.computed(function(){
+		if(self.selectedTweet() !== null && self.selectedTweet() !== undefined){
+			var text = self.selectedTweet().original;
+			var hashtags = self.selectedTweet().entities.hashtags;
+			var users = self.selectedTweet().entities.user_mentions;
+
+			//for(var x=0; x<urls.length; x++){
+			//	var url = urls[x].url;
+			//	text = text.replace(url, "<a href='" + url + "' target='_blank'>" + url + "</a>");
+			//}
+			
+			text = text.replace(/http(s?):\/\/t.co\/........../, function(url){
+				return "<a href='" + url + "' target='_blank'>" + url + "</a>";
+			});
+
+			for(var x=0; x<hashtags.length; x++){
+				text = text.replace("#" + hashtags[x].text, function(hashtag){
+
+					tag = hashtag.replace("#", "");
+					return "<a href='https://twitter.com/hashtag/" + tag + "' target='_blank'>" + hashtag + "</a>";
+				});
+			}
+
+			for(var x=0; x<users.length; x++){
+				text = text.replace("@" + users[x].screen_name, function(user){
+					noAt = user.replace("@", "");
+					return "<a href='https://twitter.com/" + noAt + "' target='_blank'>" + user + "</a>";
+				})
+			}
+
+			text = "<p>" + text + "</p>";
+			return text;
+		}
+	})
 
         self.selectedTweetTags = ko.computed(function(){
             /*
@@ -143,7 +177,10 @@ define(["ko", "jquery", "chain", "ol", "pipelineManager"], function(ko, jquery, 
 
 
         self.selectTweet = function(feature){
-		self.selectedTweet(feature.get('tweet'));
+		if(feature !== null && feature !== undefined){
+			var tweet = feature.get('tweet');
+			self.selectedTweet(tweet);
+		}
         }
 
         self.clearSelectedTweet = function(){
