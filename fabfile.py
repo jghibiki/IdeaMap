c2d1a3308dca699c8d67a3eb151160e5e24df451
jghibiki <jghibiki.games@gmail.com>
2015-10-26 18:39:38 -0500

    adds base fab file

---
+++ fabfile.py
@@ -0,0 +1,28 @@
+from fabric.api import local, lcd
+import shutil
+
+
+def start_test(app="all"):
+
+    shutil.copyfile("config/models.config.yml", "source/models/config.yml")
+    shutil.copyfile("config/streamer.config.yml", "source/streamer/config.yml")
+
+    if app == "all" or app == "analyzer":
+        with lcd("source/analyzer"):
+            print("Starting Analyzer")
+            local("python2 analyzer.py > ../../analyzer.log 2>&1 &")
+
+    if app == "all" or app == "streamer":
+        with lcd("source/streamer"):
+            print("Starting Streamer")
+            local("python2 start.py > ../../streamer.log 2>&1 &")
+
+    if app == "all" or app=="app":
+        with lcd("source/app"):
+            print("Starting Web App")
+            local("python2 start.py > ../../app.log 2>&1 &")
+
+
+
+def stop_test():
+    local("pkill python")
