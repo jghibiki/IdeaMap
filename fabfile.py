f416ee66d61f0cfac067f65b0be636908d5770dc
jghibiki <jghibiki.games@gmail.com>
2015-10-27 17:43:41 -0500

    add the ability to query the database

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,5 @@
-from fabric.api import local, lcd
+from fabric.api import local, lcd, cd
 import shutil
-
 
 def start_test(app="all"):
 
@@ -17,10 +16,11 @@ def start_test(app="all"):
             print("Starting Streamer")
             local("python2 start.py > ../../streamer.log 2>&1 &")
 
-    if app == "all" or app=="app":
+    if app == "all" or app =="app":
         with lcd("source/app"):
             print("Starting Web App")
             local("python2 start.py > ../../app.log 2>&1 &")
+
 
 def monitor_test():
     local("tail -f analyzer.log -f streamer.log -f app.log")
@@ -28,3 +28,30 @@ def monitor_test():
 
 def stop_test():
     local("pkill python")
+
+
+def query(query=None):
+    if query is None:
+        print("Must specify a query.")
+
+    from os import path
+    from peewee import PostgresqlDatabase
+    from yaml import load
+    with open(path.abspath("source/models/config.yml"), "rb") as f:
+        config = load(f)
+
+    db_config = config["database"]
+
+    db = PostgresqlDatabase(
+        db_config["db"],
+        host=db_config["url"],
+        user=db_config["username"],
+        password=db_config["password"])
+
+    print("Result:")
+    cursor = db.execute_sql(query)
+    for row in cursor.fetchall():
+        if len(row) == 1:
+            print(row[0])
+        else:
+            print(row)
